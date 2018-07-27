a3b8c796fa8b460d3747c2461fe17b0d346e990c
Florastamine <florastamine@gmail.com>
2018-07-27 10:38:21 +0700

    Updated paths

--- control.lua
+++ control.lua
@@ -12,6 +12,7 @@ local add_search_path = (function ()
   for _, v in ipairs({
     "../thirdparty/argparse/",
     "../thirdparty/moonscript/",
+    "../hardware/",
   }) do
     package.path = package.path .. ';' .. v .. "?.lua"
   end

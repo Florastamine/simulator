e7158356096d368c7a6a1f19fd0d5d3e6477d9cd
Florastamine <florastamine@gmail.com>
2018-07-27 10:42:40 +0700

    Improved and updated the build script to also generate UUID dll

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ setlocal
 set _PS_LUA_PATH=%~dp0\thirdparty\luajit
 set _PS_LPEG_PATH=%~dp0\thirdparty\lpeg
 set _PS_LFS_PATH=%~dp0\thirdparty\lfs
+set _PS_UUID_PATH=%~dp0\thirdparty\uuid
 set _PS_RUNTIME_PATH=%~dp0\runtime
 
 call:Print "Building LuaJIT"
@@ -16,6 +17,9 @@ call:Print "Building LPeg"
 call:Print "Building LuaFileSystem"
   gcc -O2 -shared -s -I%_PS_LUA_PATH%\src -L%_PS_LUA_PATH%\src -L. -o %_PS_LFS_PATH%\lfs.dll %_PS_LFS_PATH%\*.c -llua51 >nul 2>&1
 
+call:Print "Building UUID module"
+  gcc -O2 -shared -s -I%_PS_LUA_PATH%\src -L%_PS_LUA_PATH%\src -L. -o %_PS_UUID_PATH%\uuid.dll %_PS_UUID_PATH%\*.c -llua51 >nul 2>&1
+
 call:Print "Finalizing"
   rd /s /q %_PS_RUNTIME_PATH%
   mkdir %_PS_RUNTIME_PATH%
@@ -23,12 +27,14 @@ call:Print "Finalizing"
   move %_PS_LUA_PATH%\src\lua51.dll %_PS_RUNTIME_PATH% >nul 2>&1
   move %_PS_LPEG_PATH%\lpeg.dll %_PS_RUNTIME_PATH% >nul 2>&1
   move %_PS_LFS_PATH%\lfs.dll %_PS_RUNTIME_PATH% >nul 2>&1
+  move %_PS_UUID_PATH%\uuid.dll %_PS_RUNTIME_PATH% >nul 2>&1
   echo .\luajit.exe ..\control.lua ..\sample.ls >> %_PS_RUNTIME_PATH%\launch.bat
 
 call:Print "Cleaning"
   del /f /s /q  %_PS_LUA_PATH%\*.o %_PS_LUA_PATH%\*.a %_PS_LUA_PATH%\*.dll %_PS_LUA_PATH%\*.exe >nul 2>&1
 
 :Print
+title %~1, please wait...
 echo | set /p=%~1
 echo.
 exit /b
